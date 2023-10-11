b23dd37994910ae6def43404cb8f7d8a466009c6
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-11 14:45:51 +0900

    :arrow_up: :: sdk version up
    :: sdk version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hackathon.team15_android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hackathon.team15_android"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
