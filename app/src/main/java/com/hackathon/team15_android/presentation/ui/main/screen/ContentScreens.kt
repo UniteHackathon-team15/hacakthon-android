162ab67bfd23ebe546c47b48e15f8ceb0026f104
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-11 21:12:05 +0900

    :memo: :: add Library page Icon
    :: add Library page Icon

--- app/src/main/java/com/hackathon/team15_android/presentation/ui/main/screen/ContentScreens.kt
+++ app/src/main/java/com/hackathon/team15_android/presentation/ui/main/screen/ContentScreens.kt
@@ -1,13 +1,20 @@
 package com.hackathon.team15_android.presentation.ui.main.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.hackathon.team15_android.R
 
 @Composable
@@ -16,10 +23,19 @@ fun LibraryScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.white))
-            .wrapContentSize(Alignment.Center)
     ) {
-
-    }
+        Row(
+            Modifier
+                .padding(start = 26.dp, top = 23.dp, end = 312.dp)
+        ) {
+            val svgImage: Painter = painterResource(R.drawable.ic_library)
+            Image(
+                painter = svgImage,
+                contentDescription = "SVG Image",
+                modifier = Modifier.size(30.dp),
+            )
+        }
+        }
 }
 
 @Composable
