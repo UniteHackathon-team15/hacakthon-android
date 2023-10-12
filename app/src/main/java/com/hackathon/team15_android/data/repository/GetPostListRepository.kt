1a02227f19e745c8d0ec0965526227f597f111e9
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-13 01:55:11 +0900

    :memo: :: API 통신 세팅
    :: API 통신 세팅

--- app/src/main/java/com/hackathon/team15_android/data/repository/GetPostListRepository.kt
+++ app/src/main/java/com/hackathon/team15_android/data/repository/GetPostListRepository.kt
@@ -1,10 +1,19 @@
 package com.hackathon.team15_android.data.repository
 
 import com.hackathon.team15_android.data.remote.api.PostAPI
+import com.hackathon.team15_android.data.remote.dto.response.post.PostResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class GetPostListRepository @Inject constructor(
     private val postAPI: PostAPI,
 ) {
-    
+    suspend fun getPostList(): List<PostResponse> {
+        return postAPI.getPostList()
+    }
+
+
+
 }
--- app/src/main/java/com/hackathon/team15_android/presentation/viewmodel/PostListViewModel.kt
+++ app/src/main/java/com/hackathon/team15_android/presentation/viewmodel/PostListViewModel.kt
@@ -1,13 +1,33 @@
 package com.hackathon.team15_android.presentation.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hackathon.team15_android.data.remote.dto.response.post.PostResponse
+import com.hackathon.team15_android.data.repository.GetPostListRepository
+import com.hackathon.team15_android.presentation.ui.main.util.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class PostListViewModel: ViewModel() {
-    var postList = mutableListOf<PostResponse>()
-        private set
+class PostListViewModel @Inject constructor(
+    private val getPostListRepository: GetPostListRepository
+) : ViewModel() {
+    var postList by mutableStateOf<List<PostResponse>?>(null)
 
-
+    fun getPostList() = viewModelScope.launch(Dispatchers.IO) {
+        kotlin.runCatching {
+            getPostListRepository.getPostList()
+        }.onSuccess {
+            postList = it
+        }.onFailure {
+            postList = null
+            Log.d(TAG, "getPostList에러 - $it ")
+        }
+    }
 }
